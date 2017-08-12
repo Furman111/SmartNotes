c453c2c64c3909ad296d5bf26a57d801d666d1bd
Stepan Furman <stepanfurman@gmail.com>
2017-08-12 23:34:54 +0400

    almost almost final

--- app/src/main/java/ru/furman/smartnotes/utils/ImageFiles.java
+++ app/src/main/java/ru/furman/smartnotes/utils/ImageFiles.java
@@ -3,7 +3,6 @@ package ru.furman.smartnotes.utils;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,6 +11,7 @@ import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public abstract class ImageFiles {
 
@@ -85,15 +85,14 @@ public abstract class ImageFiles {
     }
 
     public static File createImageFile(Context ctx) throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.UK).format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = ctx.getFilesDir();
-        File image = File.createTempFile(
+        return File.createTempFile(
                 imageFileName,
                 ".jpg",
                 storageDir
         );
-        return image;
     }
 
 }
